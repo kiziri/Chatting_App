53510f16ca6775d4e13003e440806efdc82b61dd
kiziri <58429073+kiziri@users.noreply.github.com>
2020-08-11 02:33:20 +0900

    Update MainActivity.java
    make startBtn's click event function

--- app/src/main/java/com/example/chatting_app/MainActivity.java
+++ app/src/main/java/com/example/chatting_app/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.chatting_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button startBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startBtn = (Button) findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
