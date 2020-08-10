e692c12a26ee5ccd71530cfe48d507aeb84275ad
kiziri <58429073+kiziri@users.noreply.github.com>
2020-08-11 02:58:27 +0900

    Update code for login function

--- app/src/main/java/com/example/chatting_app/LoginActivity.java
+++ app/src/main/java/com/example/chatting_app/LoginActivity.java
@@ -2,13 +2,37 @@ package com.example.chatting_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText emailEdit, passEdit;
+
+    Intent intent;
+    FirebaseAuth mAuth;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    }
+
+    public void userSystem(View view) {
+        switch (view.getId()) {
+            case R.id.loginBtn :
+                break;
+            case R.id.registerBtn :
+                break;
+            case R.id.cancelBtn :
+                break;
+        }
     }
 }
