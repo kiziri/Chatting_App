9561ad34efbc9ef4dd40f47f9814ca51058b57e8
kiziri <58429073+kiziri@users.noreply.github.com>
2020-08-14 10:36:07 +0900

    Update for exit

---
+++ app/src/main/java/com/example/chatting_app/models/ExitMessage.java
@@ -0,0 +1,9 @@
+package com.example.chatting_app.models;
+
+public class ExitMessage  extends Message {
+
+    public ExitMessage(){
+        super.setMessageType(MessageType.EXIT);
+    }
+
+}
--- app/src/main/java/com/example/chatting_app/models/Message.java
+++ app/src/main/java/com/example/chatting_app/models/Message.java
@@ -16,6 +16,6 @@ public class Message {
     private List<String> readUserList;
 
     public enum MessageType {
-        TEXT, PHOTO
+        TEXT, PHOTO, EXIT
     }
 }
